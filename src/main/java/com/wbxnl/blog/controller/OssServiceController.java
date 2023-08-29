50b83980f221b0ad830acb3c914771a810e050bb
xiaowansheng <xiaowansheng@foxmail.com>
2023-08-30 01:53:52 +0800

    fix:修复带文件夹参数获取oss授权问题

--- src/main/java/com/wbxnl/blog/controller/OssServiceController.java
+++ src/main/java/com/wbxnl/blog/controller/OssServiceController.java
@@ -1,11 +1,9 @@
 package com.wbxnl.blog.controller;
 
-import com.aliyun.oss.OSS;
 import com.aliyun.oss.OSSClient;
 import com.aliyun.oss.common.utils.BinaryUtil;
 import com.aliyun.oss.model.MatchMode;
 import com.aliyun.oss.model.PolicyConditions;
-import com.wbxnl.blog.annotation.Api;
 import com.wbxnl.blog.common.Result;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +21,7 @@ import java.util.Map;
 @RestController
 @Slf4j
 @RequestMapping("/upload")
-@Tag(name = "OssServiceController",description = "阿里云OSS模块")
+@Tag(name = "OssServiceController", description = "阿里云OSS模块")
 public class OssServiceController {
 
     @Value("${alibaba.cloud.access-key}")
@@ -39,15 +37,15 @@ public class OssServiceController {
     private String bucket;
 
     @Value("${alibaba.cloud.oss.dir}")
-    private String dir;
+    private String directory;
 
     @GetMapping("/oss/access")
-    public Result<Map<String, String>> getOss(){
+    public Result<Map<String, String>> getOss() {
         String accessId = this.accessid; // 请填写您的AccessKeyId。
         String accessKey = this.accessKey; // 请填写您的AccessKeySecret。
         String endpoint = this.endpoint; // 请填写您的 endpoint。
         String bucket = this.bucket; // 请填写您的 bucketname 。
-        String dir = this.dir; // 用户上传文件时指定的前缀。
+        String dir = this.directory; // 用户上传文件时指定的前缀。
 //        String accessId = "LTAI5tAmSxRuhVPFxwcrmfig"; // 请填写您的AccessKeyId。
 //        String accessKey = "b6j07gIcttKidNnasKSgJ7SDyJleDo"; // 请填写您的AccessKeySecret。
 //        String endpoint = "oss-cn-chengdu.aliyuncs.com"; // 请填写您的 endpoint。
@@ -58,7 +56,7 @@ public class OssServiceController {
 //        String dir = "product"; // 用户上传文件时指定的前缀。
 
         OSSClient client = new OSSClient(endpoint, accessId, accessKey);
-        Map<String, String> respMap=new LinkedHashMap<String, String>();
+        Map<String, String> respMap = new LinkedHashMap<String, String>();
         try {
             long expireTime = 30;
             long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
@@ -101,23 +99,15 @@ public class OssServiceController {
     }
 
     @GetMapping("/oss/access/info/{dir}")
-    public Result getOssByDir(@PathVariable("dir")String dir){
+    public Result getOssByDir(@PathVariable("dir") String dir) {
+        dir = this.directory + "/" + dir;
         String accessId = this.accessid; // 请填写您的AccessKeyId。
         String accessKey = this.accessKey; // 请填写您的AccessKeySecret。
         String endpoint = this.endpoint; // 请填写您的 endpoint。
         String bucket = this.bucket; // 请填写您的 bucketname 。
-//        String dir = dir; // 用户上传文件时指定的前缀。
-//        String accessId = "LTAI5tAmSxRuhVPFxwcrmfig"; // 请填写您的AccessKeyId。
-//        String accessKey = "b6j07gIcttKidNnasKSgJ7SDyJleDo"; // 请填写您的AccessKeySecret。
-//        String endpoint = "oss-cn-chengdu.aliyuncs.com"; // 请填写您的 endpoint。
-//        String bucket = "grainmall-wbxnl"; // 请填写您的 bucketname 。
         String host = "http://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
-        // callbackUrl为 上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
-        String callbackUrl = "http://localhost:88/api/upload/callback";
-//        String dir = "product"; // 用户上传文件时指定的前缀。
-
         OSSClient client = new OSSClient(endpoint, accessId, accessKey);
-        Map<String, String> respMap=new LinkedHashMap<String, String>();
+        Map<String, String> respMap = new LinkedHashMap<String, String>();
         try {
             long expireTime = 30;
             long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
@@ -139,6 +129,10 @@ public class OssServiceController {
         } catch (Exception e) {
             // Assert.fail(e.getMessage());
             System.out.println(e.getMessage());
+        } finally {
+            if (client != null) {
+                client.shutdown();
+            }
         }
         return new Result().ok(respMap);
     }
