6dc37b3697bd6790644ae9779fe10b84d22cee80
xiaowansheng <xiaowansheng@foxmail.com>
2023-09-07 01:55:56 +0800

    add:修改个人信息接口

--- src/main/java/com/wbxnl/blog/controller/UserInfoController.java
+++ src/main/java/com/wbxnl/blog/controller/UserInfoController.java
@@ -1,16 +1,23 @@
 package com.wbxnl.blog.controller;
 
+import com.wbxnl.blog.annotation.Log;
+import com.wbxnl.blog.common.Result;
+import com.wbxnl.blog.constant.types.OperationType;
 import com.wbxnl.blog.controller.base.AbstractController;
 import com.wbxnl.blog.model.dto.UserInfoDto;
 import com.wbxnl.blog.model.entity.UserInfo;
 import com.wbxnl.blog.model.vo.UserInfoVo;
 //import io.swagger.annotations.Api;
+import com.wbxnl.blog.utils.SecurityUtils;
+import com.wbxnl.blog.validator.group.Update;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 import com.wbxnl.blog.service.UserInfoService;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -26,16 +33,27 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "UserInfoController",description = "用户信息模块")
 public class UserInfoController extends AbstractController<UserInfoService, UserInfo, UserInfoDto, UserInfoVo> {
 
-//    @Autowired
-//    UserInfoService userInfoService;
+    @Autowired
+    UserInfoService userInfoService;
 
-    // 用不到
-//    @GetMapping("")
-//    @Operation(summary = "获取当前用户的信息")
-//    public Result getInfo(){
-//        //获取当前用户id
-//        Integer userInfoId = SecurityUtils.getUserInfoId();
-//        UserInfoDto dto = userInfoService.getDto(userInfoId);
-//        return new Result<>().ok(dto);
-//    }
+    @GetMapping("")
+    @Operation(summary = "获取当前用户的信息")
+    public Result<UserInfoDto> getInfo(){
+        //获取当前用户id
+        Integer userInfoId = SecurityUtils.getUserInfoId();
+        UserInfoDto dto = userInfoService.getDto(userInfoId);
+        return new Result().ok(dto);
+    }
+
+
+    @PutMapping("/person")
+    @Operation(summary = "修改当前用户的个人信息")
+    @Log(type = OperationType.UPDATE,desc = "修改当前用户的个人信息")
+    public Result updateInformation(@Validated({Update.class}) @RequestBody UserInfoVo userInfoVo){
+        //获取当前用户id
+        Integer userInfoId = SecurityUtils.getUserInfoId();
+        userInfoVo.setId(SecurityUtils.getUserInfoId());
+        userInfoService.update(userInfoVo);
+        return new Result();
+    }
 }
