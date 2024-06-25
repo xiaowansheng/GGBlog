8f7207716e40cf16319ae6873a232506f881121d
xiaowansheng <xiaowansheng@foxmail.com>
2024-06-25 23:12:22 +0800

    feat:修改启动类名称

--- src/main/java/com/wbxnl/blog/GGBlogApplication.java
+++ src/main/java/com/wbxnl/blog/GGBlogApplication.java
@@ -2,19 +2,24 @@ package com.wbxnl.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/**
+ * @Description:博客系统
+ * @Version: 1.0
+ * @Author: xiaowansheng
+ * @Date: 2023/8/10 21:01
+ */
 //@SpringBootApplication(exclude= {SecurityAutoConfiguration.class })
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
 @EnableScheduling //开启定时任务类
 @EnableAsync //开启异步编程
-public class GgBlogApplication {
+public class GGBlogApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GgBlogApplication.class, args);
+        SpringApplication.run(GGBlogApplication.class, args);
     }
 
 }
